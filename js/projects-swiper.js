185fda1453b737a36b3f7e6c339c54d0da6e3565
Lonsie <gladishevone@gmail.com>
2023-05-15 01:31:12 +0300

    My second commit

--- js/projects-swiper.js
+++ js/projects-swiper.js
@@ -1,12 +1,23 @@
 
-var swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".mySwiper", {
+
 slidesPerView: 3,
+
 grid: {
 rows: 2,
 },
+
 spaceBetween: 32,
+
 pagination: {
 el: ".swiper-pagination",
 clickable: true,
 },
+
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
 });
